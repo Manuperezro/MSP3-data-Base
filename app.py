96d0e0aaa846ffdf1f49a1731b671898f2e7ba5e
Manuel Pedro Perez Romero <manuperezroomeroo@gmail.com>
2022-04-24 15:39:34 +0000

    templates and routes added

--- app.py
+++ app.py
@@ -1,9 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
+
 
 @app.route('/')
 def start():
-    return "Hello Wrld"
+    return render_template('base.html')
+
+
+@app.route('/create-restaurant')
+def create_restaurant():
+    return render_template('create_restaurant.html')
+
 
 if __name__ == '__main__':
+    app.jinja_env.auto_reload = True
     app.run(debug=True)
