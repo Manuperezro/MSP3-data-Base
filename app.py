7ff7fa9afe4df573a07333d179791bf57b3376dc
Manuel Pedro Perez Romero <manuperezroomeroo@gmail.com>
2022-04-25 19:42:29 +0000

    ug in insert data fixed

--- app.py
+++ app.py
@@ -1,16 +1,17 @@
 from flask import Flask, render_template, request 
 from database import db_session, init_db
-from models.restaurants import Restaurants
+from restaurants import Restaurants
 app = Flask(__name__)
 
 
-# before the first request is send, initialize the database
+app = Flask(__name__)
+
+
 @app.before_first_request
 def init():
     init_db()
+    
 
-
-# to clean the page after all the request are done move the databa sesesion
 @app.teardown_appcontext
 def shutdown_session(exception=None):
     db_session.remove()
@@ -18,7 +19,7 @@ def shutdown_session(exception=None):
 
 @app.route('/')
 def start():
-    return render_template('base.html')
+    return render_template('create_restaurant.html')
 
 
 @app.route('/create-restaurant', methods=['GET', 'POST'])
@@ -28,7 +29,7 @@ def create_restaurant():
         description = request.form.get('description')
         site_url = request.form.get('site_url')
 
-        restaurant = Restaurants(name=name, description=description, site_url=site_url,)
+        restaurant = Restaurants(name=name, description=description, site_url=site_url)
         db_session.add(restaurant)
         db_session.commit()
 
@@ -36,11 +37,6 @@ def create_restaurant():
 
     return render_template('create_restaurant.html')
 
-
-@app.route('/restaurants')
-def restaurant_list():
-    restaurants = Restaurants.query.all()
-    return render_template("restaurant.html", restaurants=restaurants)
 
 
 if __name__ == '__main__':
--- database.py
+++ database.py
@@ -1,18 +1,17 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
-import os
+import os 
 
 current_dir = os.path.dirname(__file__)
 
-engine = create_engine('sqlite:////{}/luckydraw.db'.format(current_dir), convert_unicode=True)
+engine = create_engine('sqlite:////{}/MSP3-DATA-BASE.db'.format(current_dir), convert_unicode=True)
 db_session = scoped_session(sessionmaker(autocommit=False,
                                          autoflush=False,
                                          bind=engine))
 Base = declarative_base()
 Base.query = db_session.query_property()
 
-
 def init_db():
     Base.metadata.create_all(bind=engine)
-    print("Connected succesfully to the database")
+    print('We are connected to database succesfully')
